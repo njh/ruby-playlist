f0a0a03d9d42978449dbddc664e123c842e3452c
Nicholas Humfrey <njh@aelius.com>
2018-07-13 00:17:59 +0100

    Add support for M3U playlist format

---
+++ lib/playlist/format/m3u.rb
@@ -0,0 +1,33 @@
+# Module to parse and generate M3U playlists
+module Playlist::Format::M3U
+  class << self
+    def parse(input)
+      Playlist.new do |playlist|
+        track = Playlist::Track.new
+        input.each_line do |line|
+          if line =~ /^#EXTINF:(-?\d+),\s*(.+?)\s*-\s*(.+?)\s*$/
+            track.duration = Regexp.last_match(1).to_i
+            track.duration = nil if [-1, 0].include?(track.duration)
+            track.artist = Regexp.last_match(2)
+            track.title = Regexp.last_match(3)
+          else
+            if line =~ /^[^#]\S+.+\s*$/
+              track.location = line.strip
+              playlist.add_track(track)
+            end
+            track = Playlist::Track.new
+          end
+        end
+      end
+    end
+
+    def generate(playlist)
+      text = "#EXTM3U\n"
+      playlist.tracks.each do |t|
+        text += "#EXTINF:#{t.duration.to_i},#{t.artist} - #{t.title}\n"
+        text += "#{t.location}\n"
+      end
+      text
+    end
+  end
+end
---
+++ spec/formats/m3u_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Playlist::Format::M3U do
+  describe '#parse' do
+    describe 'a tracklist of two track' do
+      let(:playlist) { Playlist::Format::M3U.parse(fixture('basic.m3u')) }
+
+      it 'parses two tracks in the tracklist' do
+        expect(playlist.tracks.count).to eq(2)
+      end
+
+      it 'parses the duration, artist, title and location from Track 1' do
+        expect(playlist.tracks[0].artist).to eq('Hot Chip')
+        expect(playlist.tracks[0].title).to eq('One One One')
+        expect(playlist.tracks[0].duration).to eq(215)
+        expect(playlist.tracks[0].location).to eq('one_one_one.mp3')
+      end
+
+      it 'parses the duration, artist, title and location from Track 2' do
+        expect(playlist.tracks[1].artist).to eq('Blur')
+        expect(playlist.tracks[1].title).to eq('Song 2')
+        expect(playlist.tracks[1].duration).to eq(110)
+        expect(playlist.tracks[1].location).to eq('song2.mp3')
+      end
+    end
+  end
+
+  describe '#generate' do
+    it 'converts each track in the playlist to a seperate line' do
+      playlist = Playlist.new
+      playlist.add_track(
+        :location => 'one_one_one.mp3',
+        :title => 'One One One',
+        :artist => 'Hot Chip',
+        :duration => 215
+      )
+      playlist.add_track(
+        :location => 'song2.mp3',
+        :title => 'Song 2',
+        :artist => 'Blur',
+        :duration => 110
+      )
+
+      text = Playlist::Format::M3U.generate(playlist)
+      expect(text).to eq(fixture('basic.m3u'))
+    end
+  end
+end
